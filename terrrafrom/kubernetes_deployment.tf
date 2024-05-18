0938a86016845b9f37f088b92cdf3588bb0d4e01
sairevanth <sairevanth@vaultedge.com>
2024-05-18 09:22:06 +0530

    teffrafrom templates updated

--- terrrafrom/kubernetes_deployment.tf
+++ terrrafrom/kubernetes_deployment.tf
@@ -1,149 +1,165 @@
-# ############## ACM Certificate #############
+############## ACM Certificate #############
 
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = var.metrics_domain_name
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.metrics_domain_name
+  validation_method = "DNS"
   
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_acm_certificate" "nginx-cert" {
-#   domain_name       = var.files_domain_name
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "nginx-cert" {
+  domain_name       = var.files_domain_name
+  validation_method = "DNS"
   
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_acm_certificate_validation" "cert" {
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
 
-# resource "aws_acm_certificate_validation" "nginx-cert" {
-#   certificate_arn         = aws_acm_certificate.nginx-cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-# }
-
-
-# resource "aws_route53_record" "cert_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
-
-#   zone_id = var.zone_id
-#   name    = each.value.name
-#   type    = each.value.type
-#   records = [each.value.record]
-#   ttl     = 60
-# }
+resource "aws_acm_certificate_validation" "nginx-cert" {
+  certificate_arn         = aws_acm_certificate.nginx-cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.nginx_cert_validation : record.fqdn]
+}
 
 
-# ###################### AWS EKS Deployment ##############################
-# resource "kubernetes_manifest" "node_exporter_deployment" {
-#   manifest = yamldecode(file("../kubernetes/deployment/node_exporter-cronjob.yaml"))
-#   depends_on = [aws_eks_node_group.my_node_group]
-# }
-# resource "kubernetes_manifest" "node_exporter_nginx_deployment" {
-#   manifest = yamldecode(file("../kubernetes/deployment/nginx.yaml"))
-#   depends_on = [aws_eks_node_group.my_node_group]
-# }
+resource "aws_route53_record" "nginx_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.nginx-cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = var.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = var.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
 
 
-# ############## Kubernetes Service with LoadBalancer #############
-
-# resource "kubernetes_service" "node-exporter-service" {
-#   metadata {
-#     name = "node-exporter"
-#     annotations = {
-#         "service.beta.kubernetes.io/aws-load-balancer-backend-protocol" = "http"
-#         "service.beta.kubernetes.io/aws-load-balancer-ssl-cert"         = aws_acm_certificate.cert.arn
-#     }
-#   }
-
-#   spec {
-#     selector = {
-#       app = "node-exporter"
-#     }
-
-#     port {
-#       protocol = "TCP"
-#       port     = 443
-#       target_port = 9100
-#     }
-
-#     type = "LoadBalancer"
-#   }
-
-#   depends_on = [aws_route53_record.cert_validation,aws_eks_node_group.my_node_group]  # Ensure Route 53 record is created before the service
-
-# }
+###################### AWS EKS Deployment ##############################
+resource "kubernetes_manifest" "node_exporter_deployment" {
+  manifest = yamldecode(file("../kubernetes/deployment/node_exporter-cronjob.yaml"))
+  depends_on = [aws_eks_node_group.my_node_group]
+}
+resource "kubernetes_manifest" "node_exporter_nginx_deployment" {
+  manifest = yamldecode(file("../kubernetes/deployment/nginx.yaml"))
+  depends_on = [aws_eks_node_group.my_node_group]
+}
 
 
-# resource "kubernetes_service" "nginx-service" {
-#   metadata {
-#     name = "nginx"
-#     annotations = {
-#         "service.beta.kubernetes.io/aws-load-balancer-backend-protocol" = "http"
-#         "service.beta.kubernetes.io/aws-load-balancer-ssl-cert"         = aws_acm_certificate.nginx-cert.arn
-#     }
-#   }
+############## Kubernetes Service with LoadBalancer #############
 
-#   spec {
-#     selector = {
-#       app = "nginx"
-#     }
+resource "kubernetes_service" "node-exporter-service" {
+  metadata {
+    name = "node-exporter"
+    annotations = {
+        "service.beta.kubernetes.io/aws-load-balancer-backend-protocol" = "http"
+        "service.beta.kubernetes.io/aws-load-balancer-ssl-cert"         = aws_acm_certificate.cert.arn
+    }
+  }
 
-#     port {
-#       protocol = "TCP"
-#       port     = 443
-#       target_port = 80
-#     }
+  spec {
+    selector = {
+      app = "node-exporter"
+    }
 
-#     type = "LoadBalancer"
-#   }
+    port {
+      protocol = "TCP"
+      port     = 443
+      target_port = 9100
+    }
 
-# }
+    type = "LoadBalancer"
+  }
 
+  depends_on = [aws_route53_record.cert_validation,aws_eks_node_group.my_node_group]  # Ensure Route 53 record is created before the service
 
-# # Create a Route 53 record for the load balancer URL
-# data "aws_elb" "node_exporter_lb" {
-#     name = split("-", kubernetes_service.node-exporter-service.status.0.load_balancer.0.ingress.0.hostname)[0]
-# }
+}
 
 
-# data "aws_elb" "nginx_exporter_lb" {
-#     name = split("-", kubernetes_service.nginx-service.status.0.load_balancer.0.ingress.0.hostname)[0]
-# }
+resource "kubernetes_service" "nginx-service" {
+  metadata {
+    name = "nginx"
+    annotations = {
+        "service.beta.kubernetes.io/aws-load-balancer-backend-protocol" = "http"
+        "service.beta.kubernetes.io/aws-load-balancer-ssl-cert"         = aws_acm_certificate.nginx-cert.arn
+    }
+  }
+
+  spec {
+    selector = {
+      app = "nginx"
+    }
+
+    port {
+      protocol = "TCP"
+      port     = 443
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+
+}
 
 
-# resource "aws_route53_record" "node_exporter_lb" {
-#   zone_id         = var.zone_id
-#   name            = var.metrics_domain_name  # Replace with your domain name
-#   type            = "A"
-#  alias {
-#     name                   = data.aws_elb.node_exporter_lb.dns_name
-#     zone_id                = data.aws_elb.node_exporter_lb.zone_id
-#     evaluate_target_health = false
-#   }
-# }
+# Create a Route 53 record for the load balancer URL
+data "aws_elb" "node_exporter_lb" {
+    name = split("-", kubernetes_service.node-exporter-service.status.0.load_balancer.0.ingress.0.hostname)[0]
+}
+
+
+data "aws_elb" "nginx_exporter_lb" {
+    name = split("-", kubernetes_service.nginx-service.status.0.load_balancer.0.ingress.0.hostname)[0]
+}
+
+
+resource "aws_route53_record" "node_exporter_lb" {
+  zone_id         = var.zone_id
+  name            = var.metrics_domain_name  # Replace with your domain name
+  type            = "A"
+ alias {
+    name                   = data.aws_elb.node_exporter_lb.dns_name
+    zone_id                = data.aws_elb.node_exporter_lb.zone_id
+    evaluate_target_health = false
+  }
+}
 
 
 
-# resource "aws_route53_record" "nginx_lb" {
-#   zone_id         = var.zone_id
-#   name            = var.files_domain_name  # Replace with your domain name
-#   type            = "A"
-#  alias {
-#     name                   = data.aws_elb.nginx_exporter_lb.dns_name
-#     zone_id                = data.aws_elb.nginx_exporter_lb.zone_id
-#     evaluate_target_health = false
-#   }
-# }
+resource "aws_route53_record" "nginx_lb" {
+  zone_id         = var.zone_id
+  name            = var.files_domain_name  # Replace with your domain name
+  type            = "A"
+ alias {
+    name                   = data.aws_elb.nginx_exporter_lb.dns_name
+    zone_id                = data.aws_elb.nginx_exporter_lb.zone_id
+    evaluate_target_health = false
+  }
+}
